b6adad2456345eb8839183c49940a136603844a4
h0ngg0m <jaehongbyeon98@gmail.com>
2024-05-16 13:10:36 +0900

    :tada: init

---
+++ src/test/java/hello/springbasic2/SpringBasic2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springbasic2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasic2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
